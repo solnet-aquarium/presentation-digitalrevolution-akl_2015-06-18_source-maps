1d471e27832f9bcb45f25a6eea70d4645f5c417a
Michael Robinson <michael.robinson@solnet.co.nz>
2015-06-17 11:01:25 +1200

    Updated Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,37 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';',
+        sourceMap: true
+      },
+      dist: {
+        src: [
+          'src/public/bower_components/jquery/jquery.js',
+          'src/public/bower_components/angular/angular.js',
+          'src/public/core.js'
+        ],
+        dest: 'build/concat.js'
+      }
+    },
     uglify: {
       all_src: {
         options: {
           sourceMap: true,
-          sourceMapName: 'sourceMap.map'
+          sourceMapName: 'src/public/all.js.map',
+          sourceMapIn: 'build/concat.js.map'
         },
-        src: 'src/**/*.js',
-        dest: 'composite.all.min.js'
+        src: [
+          'build/concat.js'
+        ],
+        dest: 'src/public/all.js'
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['uglify']);
-}
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
